d935f26b8abfc8df0086ef5483642dbbff906a20
Your Name <you@example.com>
2022-11-17 21:01:39 +0100

    Spring Inicjalizer

---
+++ BackendMagisterka/src/main/java/com/tokarskia/BackendMagisterka/BackendMagisterkaApplication.java
@@ -0,0 +1,13 @@
+package com.tokarskia.BackendMagisterka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMagisterkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMagisterkaApplication.class, args);
+	}
+
+}
---
+++ BackendMagisterka/src/test/java/com/tokarskia/BackendMagisterka/BackendMagisterkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tokarskia.BackendMagisterka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMagisterkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
