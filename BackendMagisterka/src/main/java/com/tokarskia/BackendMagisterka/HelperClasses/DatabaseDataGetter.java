23ce681fc38356ebf96d50e943ad9fdb867af6e6
Your Name <you@example.com>
2022-11-20 19:29:18 +0100

    filled class structure

--- BackendMagisterka/src/main/java/com/tokarskia/BackendMagisterka/HelperClasses/DatabaseDataGetter.java
+++ BackendMagisterka/src/main/java/com/tokarskia/BackendMagisterka/HelperClasses/DatabaseDataGetter.java
@@ -1,4 +1,20 @@
 package com.tokarskia.BackendMagisterka.HelperClasses;
 
+import org.springframework.data.util.Pair;
+
+import javax.xml.transform.Result;
+import java.sql.*;
+
 public class DatabaseDataGetter {
+    static public Pair<String, String> getTexts(){
+        try{
+            Connection conn = DriverManager.getConnection("connectionString");
+            Statement stmt = conn.createStatement();
+            String sql = "select costam";
+            ResultSet rl = stmt.executeQuery(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return Pair.of("a", "b");
+    }
 }
