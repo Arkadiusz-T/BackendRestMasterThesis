cc6b4b0a00e28afa4b3e2f8aa199fa41a44ccffb
Your Name <you@example.com>
2022-11-24 21:47:05 +0100

    fix import

--- BackendMagisterka/src/main/java/com/tokarskia/BackendMagisterka/controller/ResponseController.java
+++ BackendMagisterka/src/main/java/com/tokarskia/BackendMagisterka/controller/ResponseController.java
@@ -7,7 +7,6 @@ import org.springframework.data.util.Pair;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.LinkedList;
 import java.util.List;
 
